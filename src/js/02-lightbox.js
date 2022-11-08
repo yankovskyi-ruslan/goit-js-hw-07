eb74d21e6ac1bb0b00c3fa06eaf8805e90242bb6
yankovskyi-ruslan <tital3108@gmail.com>
2022-11-08 16:57:39 +0200

    Закінчує виконання завдання 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+
+function createGallaryMarcap(arr) {
+    return arr.map(({ original, preview, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    }).join("");
+}
+
+galleryRef.insertAdjacentHTML('afterbegin', createGallaryMarcap(galleryItems));
+
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
 
 console.log(galleryItems);
